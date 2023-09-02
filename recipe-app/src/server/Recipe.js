440c84a094c653028594c469a7f377e97b54dcbb
Jamie Conway <jamie@jpconway.com>
2023-09-02 11:13:54 -0500

    update ingredient type

--- recipe-app/src/server/Recipe.js
+++ recipe-app/src/server/Recipe.js
@@ -11,7 +11,7 @@ const RecipeSchema = new mongoose.Schema({
     required: true,
   },
   ingredients: {
-    type: [String],
+    type: [{ id: String, amount: String, unit: String }],
     required: true,
   },
   instructions: {
