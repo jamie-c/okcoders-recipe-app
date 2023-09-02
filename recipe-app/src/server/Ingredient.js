f9fa0a1c9a91e574a6e425e7cb2ea7530468c9fa
Jamie Conway <jamie@jpconway.com>
2023-09-02 11:26:56 -0500

    add fdcId

--- recipe-app/src/server/Ingredient.js
+++ recipe-app/src/server/Ingredient.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 const IngredientSchema = new mongoose.Schema({
-  // [] TODO: Add the ingredient schema here
-  name: {
-    type: String,
-    required: true,
-  },
+    // [] TODO: Add the ingredient schema here
+    // [] TODO: Determine which nutrient fields, if any, want to include in the schema
+    name: {
+      type: String,
+      required: true,
+    },
+    fdcId: {
+      type: String,
+    },
 });
 
 export default mongoose.models.Ingredient ||
