b632aa8235614167cf44e5ad18a788b2c26c0032
Jamie Conway <jamie@jpconway.com>
2023-09-09 10:53:38 -0500

    create ingredients api route

---
+++ recipe-app/src/pages/api/ingredients/index.js
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import Ingredient from '@/server/Ingredient'
+import dbConnect from '@/server/db'
+
+export default async function handler(req, res) {
+    await dbConnect()
+    const { name } = req.headers
+
+    if (req.method === 'GET') {
+        let ingredients
+        if (name) {
+            ingredients = await Ingredient.find(
+                // return items that have a name that contains the name query
+                { name: { $regex: name, $options: 'i' } },
+                { name: true },
+                { limit: 10, sort: { name: 'asc' } }
+            )
+        } else {
+            ingredients = await Ingredient.find(
+                {},
+                { name: true },
+                { limit: 10, sort: { name: 'asc' } }
+            )
+        }
+
+        res.status(200).json({ ingredients })
+    } else if (req.method === 'POST') {
+        const newIngredient = new Ingredient(req.body)
+        await newIngredient.save()
+        res.status(201).json(newIngredient)
+    } else {
+        res.status(405).json({ message: 'Method not allowed' })
+    }
+}
