1969c88fb07eb7de3f69f69685419dbb041ab8fe
Jamie Conway <jamie@jpconway.com>
2023-09-10 16:34:15 -0500

    update types

--- recipe-app/src/server/Recipe.js
+++ recipe-app/src/server/Recipe.js
@@ -1,56 +1,56 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const RecipeSchema = new mongoose.Schema({
-  // [] TODO: Finalize the recipe schema here
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  ingredients: {
-    type: [{ id: String, amount: String, unit: String }],
-    required: true,
-  },
-  instructions: {
-    type: [String],
-    required: true,
-  },
-  prepTime: {
-    type: Number,
-    required: true,
-  },
-  cookTime: {
-    type: Number,
-    required: true,
-  },
-  servings: {
-    type: { amount: Number, unit: String},
-    required: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  tags: {
-    type: [String],
-    required: true,
-  },
-  createdBy: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+    // [] TODO: Finalize the recipe schema here
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    ingredients: {
+        type: [{ id: String, name: String, amount: String, unit: String }],
+        required: true,
+    },
+    instructions: {
+        type: [String],
+        required: true,
+    },
+    prepTime: {
+        type: { hours: Number, minutes: Number },
+        required: true,
+    },
+    cookTime: {
+        type: { hours: Number, minutes: Number },
+        required: true,
+    },
+    servings: {
+        type: { amount: Number, unit: String },
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+        required: true,
+    },
+    createdBy: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
 
 export default mongoose.models.Recipe ||
-  mongoose.model("Recipe", RecipeSchema, "recipes");
+    mongoose.model('Recipe', RecipeSchema, 'recipes')
