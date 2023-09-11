53f752d9e68d4c6e6642b5534ea29817795e49d5
Jamie Conway <jamie@jpconway.com>
2023-09-11 12:24:08 -0500

    add optional maxRecipes in header

--- recipe-app/src/pages/api/recipes/index.js
+++ recipe-app/src/pages/api/recipes/index.js
@@ -3,12 +3,25 @@ import Recipe from '@/server/Recipe'
 
 export default async function handler(req, res) {
     await dbConnect()
+    const { maxRecipes } = req.headers
 
     if (req.method === 'POST') {
         const recipe = await Recipe.create(req.body)
         res.status(201).json(recipe)
-    } else {
-        const recipes = await Recipe.find({})
+    } else if (req.method === 'GET') {
+        let recipes
+        if (maxRecipes) {
+            recipes = await Recipe.find(
+                {},
+                {},
+                { limit: parseInt(maxRecipes), sort: { name: 'asc' } }
+            )
+        } else {
+            recipes = await Recipe.find({})
+        }
+
         res.status(200).json(recipes)
+    } else {
+        res.status(405).json({ message: 'Method not allowed' })
     }
 }
