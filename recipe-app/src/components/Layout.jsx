ff18dc3115cd9348fe7da0d7c373f858f0f3f9e4
Jamie Conway <jamie@jpconway.com>
2023-09-11 10:57:36 -0500

    add ThemeProvider

--- recipe-app/src/components/Layout.jsx
+++ recipe-app/src/components/Layout.jsx
@@ -1,11 +1,17 @@
+import { inter, theme } from '@/lib/theme'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
 import Header from './Header'
 
 export default function Layout({ children }) {
     return (
-        <div>
-            <Header />
-            <main>{children}</main>
-            {/* <Footer /> */}
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className={`${inter.className}`}>
+                <Header />
+                <main>{children}</main>
+                {/* <Footer /> */}
+            </div>
+        </ThemeProvider>
     )
 }
