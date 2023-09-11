3fec72d9590b1578e74fd7397e2cc37c0e0386ac
Jamie Conway <jamie@jpconway.com>
2023-09-11 12:23:38 -0500

    Create link to recipes

--- recipe-app/src/pages/index.js
+++ recipe-app/src/pages/index.js
@@ -1,11 +1,48 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Stack } from '@mui/material'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+    const [recipes, setRecipes] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const fetchRecipes = async () => {
+        setLoading(true)
+        const res = await fetch('/api/recipes', {
+            headers: {
+                maxRecipes: 10,
+            },
+        })
+        const data = await res.json()
+        setRecipes(data)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchRecipes()
+    }, [])
+
     return (
         <>
-     
+            <h1>Home</h1>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <Stack>
+                    {recipes.map((recipe) => (
+                        <Link
+                            style={{
+                                textDecoration: 'none',
+                                color: '#010b8b',
+                            }}
+                            href={`/recipes/${recipe._id}`}
+                            key={recipe._id}
+                        >
+                            {recipe.name}
+                        </Link>
+                    ))}
+                </Stack>
+            )}
         </>
     )
 }
