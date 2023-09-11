6949f8c4edbd3c4fe1afc1f207231ff5057a0f6c
Jamie Conway <jamie@jpconway.com>
2023-09-11 10:58:11 -0500

    create FullWidthHeaderImage.jsx

---
+++ recipe-app/src/components/FullWidthHeaderImage.jsx
@@ -0,0 +1,32 @@
+import { theme } from '@/lib/theme'
+import Image from 'next/image'
+
+const FullWidthHeaderImage = ({ recipe, children }) => (
+    <div style={{ position: 'relative', width: '100vw', height: '65vh' }}>
+        <Image
+            style={{ objectFit: 'cover', borderRadius: 6 }}
+            src={recipe.imageUrl}
+            alt={recipe.name}
+            fill
+        />
+        <div
+            style={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                textAlign: 'center',
+                background: `linear-gradient(to bottom, ${theme.palette.primary.main}, transparent)`,
+            }}
+        >
+            {children}
+        </div>
+    </div>
+)
+
+export default FullWidthHeaderImage
