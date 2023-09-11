1c667baf1fa55f583746c76c803ab5161fbfc954
Jamie Conway <jamie@jpconway.com>
2023-09-11 10:57:57 -0500

    create RecipeTitle.jsx

---
+++ recipe-app/src/components/RecipeTitle.jsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material'
+
+const RecipeTitle = ({ children }) => (
+    <div
+        style={{
+            position: 'relative',
+            width: '100%',
+            margin: '30px 0',
+        }}
+    >
+        <Typography
+            variant="h2"
+            fontWeight="bold"
+            fontStyle="italic"
+            textTransform="uppercase"
+        >
+            {children}
+        </Typography>
+    </div>
+)
+
+export default RecipeTitle
