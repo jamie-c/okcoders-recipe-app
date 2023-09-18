e705bd81d8d47b118db54eac0fef23b57a9e82c4
Jamie Conway <jamie@jpconway.com>
2023-09-18 11:40:30 -0500

    add fdc-search endpoint

---
+++ recipe-app/src/pages/api/ingredients/fdc-search.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        console.log('running fdc-search')
+        const { name, maxIngredients } = req.headers
+        const nameQuery = name.replace(' ', '%20')
+        const maxIngredientsQuery = maxIngredients ? maxIngredients : 10
+        const ingredients = await fetch(
+            `https://api.nal.usda.gov/fdc/v1/foods/search?query=${nameQuery}&dataType=Foundation,SR%20Legacy&pageSize=${maxIngredientsQuery}&sortBy=dataType.keyword&sortOrder=asc&api_key=${process.env.FDC_API_KEY}`
+        )
+        const data = await ingredients.json()
+        res.status(200).json({ ingredients: data })
+    }
+}
