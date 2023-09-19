ca7f0a72a64cd646db96b69eeae9e86678f91db9
Jamie Conway <jamie@jpconway.com>
2023-09-19 10:00:28 -0500

    reduce maxIngredients default to 5, return res

--- recipe-app/src/pages/api/ingredients/fdc-search.js
+++ recipe-app/src/pages/api/ingredients/fdc-search.js
@@ -3,11 +3,11 @@ export default async function handler(req, res) {
         console.log('running fdc-search')
         const { name, maxIngredients } = req.headers
         const nameQuery = name.replace(' ', '%20')
-        const maxIngredientsQuery = maxIngredients ? maxIngredients : 10
+        const maxIngredientsQuery = maxIngredients ? maxIngredients : 5
         const ingredients = await fetch(
             `https://api.nal.usda.gov/fdc/v1/foods/search?query=${nameQuery}&dataType=Foundation,SR%20Legacy&pageSize=${maxIngredientsQuery}&sortBy=dataType.keyword&sortOrder=asc&api_key=${process.env.FDC_API_KEY}`
         )
         const data = await ingredients.json()
-        res.status(200).json({ ingredients: data })
+        return res.status(200).json({ ingredients: data })
     }
 }
