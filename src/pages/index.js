bc5c7f3923eea40b29e845f940133b16553e260d
Karen Campbell <karenruthcampbelll@gmail.com>
2023-09-24 18:21:09 -0500

    Updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,15 @@
-import { Stack } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import {
+    Box,
+    Card,
+    CardContent,
+    CardMedia,
+    Grid,
+    IconButton,
+    InputAdornment,
+    TextField,
+    Typography,
+} from '@mui/material'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
@@ -23,26 +34,69 @@ export default function Home() {
     }, [])
 
     return (
-        <>
-            <h1>Home</h1>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <Stack>
-                    {recipes?.map((recipe) => (
-                        <Link
-                            style={{
-                                textDecoration: 'none',
-                                color: '#010b8b',
-                            }}
-                            href={`/recipes/${recipe._id}`}
-                            key={recipe._id}
-                        >
-                            {recipe.name}
-                        </Link>
-                    ))}
-                </Stack>
-            )}
-        </>
+        <Box
+            p={2}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            height="90vh"
+        >
+            <TextField
+                fullWidth
+                placeholder="Search for a Recipe"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <IconButton>
+                                <SearchIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+                variant="outlined"
+                size="large"
+                style={{ marginBottom: '16px' }}
+            />
+            <Typography
+                variant="h3"
+                gutterBottom
+                style={{
+                    fontFamily: 'Comfortaa',
+                    marginTop: '100px',
+                }}
+            >
+                Featured Recipes
+            </Typography>
+            <Grid container spacing={2}>
+                {loading ? (
+                    <Typography>Loading...</Typography>
+                ) : (
+                    recipes.slice(0, 3).map((recipe) => (
+                        <Grid item xs={12} sm={4} key={recipe._id}>
+                            <Card style={{ height: '100%' }}>
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image={recipe.imageUrl}
+                                    alt={recipe.name}
+                                />
+                                <CardContent>
+                                    <Typography
+                                        variant="h5"
+                                        style={{ fontFamily: 'Comfortaa' }}
+                                    >
+                                        {recipe.name}
+                                    </Typography>
+                                    <Link href={`/recipes/${recipe._id}`}>
+                                        View Recipe
+                                    </Link>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))
+                )}
+            </Grid>
+        </Box>
     )
 }
