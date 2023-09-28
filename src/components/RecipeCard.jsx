8cd9282034418b5a46ae14d1e4b64dd523e8446a
lily-oliver <108649511+lily-oliver@users.noreply.github.com>
2023-09-27 19:29:10 -0500

    added theme.js & wrapped app, del globals.css code

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,77 +1,70 @@
 import FullWidthHeaderImageLoading from '@/components/FullWidthHeaderImageLoading'
-import {
-    Card,
-    CardContent,
-    List,
-    ListItem,
-    ListItemText,
-    Typography,
-} from '@mui/material'
+import { Card, CardContent, List, ListItem, ListItemText, Typography } from '@mui/material';
 import RecipeCardHeaderImage from './RecipeCardHeaderImage'
 
 function RecipeCard({ recipe, loading }) {
     return (
-        <Card style={styles.card}>
-            {/* Ingredients */}
-            <div style={styles.ingredients}>
-                <Typography variant="h5">Ingredients</Typography>
-                <List>
-                    {recipe &&
-                        Array.isArray(recipe.ingredients) &&
-                        recipe.ingredients.map((ingredient) => (
-                            <ListItem key={ingredient._id}>
-                                <ListItemText>
-                                    {ingredient.amount} {ingredient.unit}{' '}
-                                    {ingredient.name}
-                                </ListItemText>
-                            </ListItem>
-                        ))}
-                </List>
-            </div>
+            <Card style={styles.card}>
+                {/* Ingredients */}
+                <div style={styles.ingredients}>
+                    <Typography variant="h5">Ingredients</Typography>
+                    <List>
+                        {recipe &&
+                            Array.isArray(recipe.ingredients) &&
+                            recipe.ingredients.map((ingredient) => (
+                                <ListItem key={ingredient._id}>
+                                    <ListItemText>
+                                        {ingredient.amount} {ingredient.unit}{' '}
+                                        {ingredient.name}
+                                    </ListItemText>
+                                </ListItem>
+                            ))}
+                    </List>
+                </div>
 
-            {/* Instructions */}
-            <CardContent style={styles.instructions}>
-                <container style={styles.recipeNameContainer}>
-                    <Typography variant="h5" style={styles.recipeName}>
-                        {recipe.name}
-                    </Typography>
-                </container>
-                {/* Render FullWidthHeaderImageLoading if loading */}
-                {recipe.loading && (
-                    <FullWidthHeaderImageLoading recipe={recipe}>
-                        <Typography variant="h1" color="white" width="75%">
-                            {/* Loading content */}
+                {/* Instructions */}
+                <CardContent style={styles.instructions}>
+                    <container style={styles.recipeNameContainer}>
+                        <Typography variant="h5" style={styles.recipeName}>
+                            {recipe.name}
                         </Typography>
-                    </FullWidthHeaderImageLoading>
-                )}
+                    </container>
+                    {/* Render FullWidthHeaderImageLoading if loading */}
+                    {recipe.loading && (
+                        <FullWidthHeaderImageLoading recipe={recipe}>
+                            <Typography variant="h1" color="white" width="75%">
+                                {/* Loading content */}
+                            </Typography>
+                        </FullWidthHeaderImageLoading>
+                    )}
 
-                {/* Render FullWidthHeaderImage if not loading */}
-                {!recipe.loading && (
-                    <RecipeCardHeaderImage recipe={recipe} loading={loading}>
-                        <Typography
-                            variant="h1"
-                            color="white"
-                            sx={{
-                                textTransform: 'uppercase',
-                                fontWeight: 'bold',
-                                fontSize: {
-                                    xs: '2rem',
-                                    sm: '3rem',
-                                    md: '4rem',
-                                    lg: '5rem',
-                                    xl: '6rem',
-                                },
-                            }}
-                        ></Typography>
-                    </RecipeCardHeaderImage>
-                )}
-                {recipe.instructions.map((instruction, index) => (
-                    <p key={`${recipe._id}-instruction-${index}`}>
-                        {instruction}
-                    </p>
-                ))}
-            </CardContent>
-        </Card>
+                    {/* Render FullWidthHeaderImage if not loading */}
+                    {!recipe.loading && (
+                        <RecipeCardHeaderImage recipe={recipe} loading={loading}>
+                            <Typography
+                                variant="h1"
+                                color="white"
+                                sx={{
+                                    textTransform: 'uppercase',
+                                    fontWeight: 'bold',
+                                    fontSize: {
+                                        xs: '2rem',
+                                        sm: '3rem',
+                                        md: '4rem',
+                                        lg: '5rem',
+                                        xl: '6rem',
+                                    },
+                                }}
+                            ></Typography>
+                        </RecipeCardHeaderImage>
+                    )}
+                    {recipe.instructions.map((instruction, index) => (
+                        <p key={`${recipe._id}-instruction-${index}`}>
+                            {instruction}
+                        </p>
+                    ))}
+                </CardContent>
+            </Card>
     )
 }
 
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,20 @@
-import Layout from '@/components/Layout'
-import '@/styles/globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import Layout from '@/components/Layout';
+import '@/styles/globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import { theme } from '@/lib/theme';
+
 
 export default function App({ Component, pageProps }) {
     return (
+      <ThemeProvider theme={theme}>
         <ClerkProvider {...pageProps}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </ClerkProvider>
-    )
-}
-// 
+      </ThemeProvider>
+    );
+  }
+  
