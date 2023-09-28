f9e6dbe31b794bbe4c60fc9029dc07a3d5d639a1
Karen Campbell <karenruthcampbelll@gmail.com>
2023-09-28 18:15:05 -0500

    Created Feeling Hungry Button

--- src/components/NavigationDrawer.jsx
+++ src/components/NavigationDrawer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     Button,
     Drawer,
@@ -6,49 +5,96 @@ import {
     ListItem,
     ListItemText,
     Paper,
+    Typography,
 } from '@mui/material'
-import { useRouter } from 'next/router' // Import the useRouter hook
+import Link from 'next/link'
 
 function NavigationDrawer({ open, onClose }) {
-    const router = useRouter() // Initialize the useRouter hook
-
-    const navigateTo = (path) => {
-        router.push(path) // Use the push method to navigate to the specified path
-        onClose() // Close the drawer after navigation
-    }
-
     return (
         <Drawer anchor="right" open={open} onClose={onClose}>
             {/* Use Paper to control the width of the Drawer */}
             <Paper style={styles.NavigationDrawer}>
                 <List>
-                    {/* Use the ListItem component correctly */}
-                    <ListItem
-                        button
-                        onClick={() => navigateTo('/recipe-index')}
-                    >
-                        {/* Use the Button component inside ListItem */}
-                        <Button>
-                            <ListItemText primary="Browse Recipes" />
-                        </Button>
+                    {/* Use the Button component for clickable items */}
+                    <ListItem Button onClick={onClose}>
+                        <Link href="/">
+                            <Button>
+                                <ListItemText
+                                    primary={
+                                        <Typography
+                                            style={styles.firstListItemText}
+                                        >
+                                            Home
+                                        </Typography>
+                                    }
+                                />
+                            </Button>
+                        </Link>
                     </ListItem>
-                    <ListItem button onClick={() => navigateTo('/ingredients')}>
-                        <Button>
-                            <ListItemText primary="Browse Ingredients" />
-                        </Button>
+                    <ListItem Button onClick={onClose}>
+                        <Link href="/">
+                            <Button>
+                                <ListItemText
+                                    primary={
+                                        <Typography style={styles.listItemText}>
+                                            My Recipes
+                                        </Typography>
+                                    }
+                                />
+                            </Button>
+                        </Link>
                     </ListItem>
-                    <ListItem button onClick={() => navigateTo('/my-recipes')}>
-                        <Button>
-                            <ListItemText primary="My Recipes" />
-                        </Button>
+                    <ListItem Button onClick={onClose}>
+                        <Link href="/recipes">
+                            <Button>
+                                <ListItemText
+                                    primary={
+                                        <Typography style={styles.listItemText}>
+                                            Add a Recipe
+                                        </Typography>
+                                    }
+                                />
+                            </Button>
+                        </Link>
                     </ListItem>
-                    <ListItem button onClick={() => navigateTo('/recipes')}>
-                        <Button>
-                            <ListItemText primary="Add New Recipe" />
-                        </Button>
+                    <ListItem Button onClick={onClose}>
+                        <Link href="/recipe-index">
+                            <Button>
+                                <ListItemText
+                                    primary={
+                                        <Typography style={styles.listItemText}>
+                                            All Recipes
+                                        </Typography>
+                                    }
+                                />
+                            </Button>
+                        </Link>
                     </ListItem>
-                    <ListItem button onClick={() => navigateTo('/recipes')}>
-                        {/* If this is a placeholder, you may want to add text or content */}
+                    <ListItem Button onClick={onClose}>
+                        <Link href="/">
+                            <Button>
+                                <ListItemText
+                                    primary={
+                                        <Typography style={styles.listItemText}>
+                                            All Ingredients
+                                        </Typography>
+                                    }
+                                />
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem Button onClick={onClose}>
+                        <Link href="/">
+                            <Button>
+                                <ListItemText
+                                    primary={
+                                        <Typography style={styles.listItemText}>
+                                            Advanced Search
+                                        </Typography>
+                                    }
+                                />
+                            </Button>
+                        </Link>
                     </ListItem>
                     {/* Add more navigation links as needed */}
                 </List>
@@ -59,85 +105,18 @@ function NavigationDrawer({ open, onClose }) {
 
 const styles = {
     NavigationDrawer: {
-        width: '500px', // Adjust the width as needed
+        width: '400px', // Adjust the width as needed
+        height: '100vh',
+    },
+    listItemText: {
+        fontSize: '20px',
+        // fontWeight: 'bold',
+    },
+    firstListItemText: {
+        fontSize: '20px',
+        fontWeight: 'bold',
+        color: '#de5006',
     },
 }
-=======
-import React from 'react';
-import { Drawer, List, ListItem, ListItemText, Button, Paper, Typography } from '@mui/material';
-import Link from 'next/link';
-
-function NavigationDrawer({ open, onClose }) {
-  return (
-    <Drawer anchor="right" open={open} onClose={onClose}>
-      {/* Use Paper to control the width of the Drawer */}
-      <Paper style={styles.NavigationDrawer}>
-        <List>
-          {/* Use the Button component for clickable items */}
-          <ListItem Button onClick={onClose}>
-            <Link href="/">
-              <Button>
-                <ListItemText primary={<Typography style={styles.firstListItemText}>Home</Typography>} />
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem Button onClick={onClose}>
-            <Link href="/">
-              <Button>
-                <ListItemText primary={<Typography style={styles.listItemText}>My Recipes</Typography>} />
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem Button onClick={onClose}>
-            <Link href="/recipes">
-              <Button>
-                <ListItemText primary={<Typography style={styles.listItemText}>Add a Recipe</Typography>} />
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem Button onClick={onClose}>
-            <Link href="/recipe-index">
-              <Button>
-                <ListItemText primary={<Typography style={styles.listItemText}>All Recipes</Typography>} />
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem Button onClick={onClose}>
-            <Link href="/">
-              <Button>
-                <ListItemText primary={<Typography style={styles.listItemText}>All Ingredients</Typography>} />
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem Button onClick={onClose}>
-            <Link href="/">
-              <Button>
-                <ListItemText primary={<Typography style={styles.listItemText}>Advanced Search</Typography>} />
-              </Button>
-            </Link>
-          </ListItem>
-          {/* Add more navigation links as needed */}
-        </List>
-      </Paper>
-    </Drawer>
-  );
-}
-
-const styles = {
-  NavigationDrawer: {
-    width: '400px', // Adjust the width as needed
-    height: '100vh',
-  },
-  listItemText: {
-    fontSize: '20px',
-    // fontWeight: 'bold',
-  },
-  firstListItemText: {
-    fontSize: '20px',
-    fontWeight: 'bold',
-    color: '#de5006',
-  }
-};
->>>>>>> c0a26588ee2917d07a9bf8eb4d786c6a947d1860
 
 export default NavigationDrawer
