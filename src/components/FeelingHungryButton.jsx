211f29a3462a5e6955bb8676272dc866a427f644
lily-oliver <108649511+lily-oliver@users.noreply.github.com>
2023-09-28 17:22:21 -0500

    feeling hungry button

---
+++ src/components/FeelingHungryButton.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material'
+import Link from 'next/link'
+
+const FeelingHungryButton = () => {
+    return (
+        <Link href="/api/recipes/random" passHref>
+            <Button variant="contained" color="primary">
+                I'm Feeling Hungry
+            </Button>
+        </Link>
+    )
+}
+
+export default FeelingHungryButton
