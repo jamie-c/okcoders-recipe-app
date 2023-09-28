3b615e25b2a3026f2a72e7016bf04a7ab23ac3a0
lily-oliver <108649511+lily-oliver@users.noreply.github.com>
2023-09-28 14:37:52 -0500

    Updated navigation drawer links

--- src/components/NavigationDrawer.jsx
+++ src/components/NavigationDrawer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Drawer, List, ListItem, ListItemText, Button, Paper } from '@mui/material';
+import { Drawer, List, ListItem, ListItemText, Button, Paper, Typography } from '@mui/material';
+import Link from 'next/link';
 
 function NavigationDrawer({ open, onClose }) {
   return (
@@ -9,14 +10,46 @@ function NavigationDrawer({ open, onClose }) {
         <List>
           {/* Use the Button component for clickable items */}
           <ListItem Button onClick={onClose}>
-            <Button>
-              <ListItemText primary="Link 1" />
-            </Button>
+            <Link href="/">
+              <Button>
+                <ListItemText primary={<Typography style={styles.firstListItemText}>Home</Typography>} />
+              </Button>
+            </Link>
           </ListItem>
           <ListItem Button onClick={onClose}>
-            <Button>
-              <ListItemText primary="Link 2" />
-            </Button>
+            <Link href="/">
+              <Button>
+                <ListItemText primary={<Typography style={styles.listItemText}>My Recipes</Typography>} />
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem Button onClick={onClose}>
+            <Link href="/recipes">
+              <Button>
+                <ListItemText primary={<Typography style={styles.listItemText}>Add a Recipe</Typography>} />
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem Button onClick={onClose}>
+            <Link href="/recipe-index">
+              <Button>
+                <ListItemText primary={<Typography style={styles.listItemText}>All Recipes</Typography>} />
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem Button onClick={onClose}>
+            <Link href="/">
+              <Button>
+                <ListItemText primary={<Typography style={styles.listItemText}>All Ingredients</Typography>} />
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem Button onClick={onClose}>
+            <Link href="/">
+              <Button>
+                <ListItemText primary={<Typography style={styles.listItemText}>Advanced Search</Typography>} />
+              </Button>
+            </Link>
           </ListItem>
           {/* Add more navigation links as needed */}
         </List>
@@ -27,8 +60,18 @@ function NavigationDrawer({ open, onClose }) {
 
 const styles = {
   NavigationDrawer: {
-    width: '500px', // Adjust the width as needed
+    width: '400px', // Adjust the width as needed
+    height: '100vh',
   },
+  listItemText: {
+    fontSize: '20px',
+    // fontWeight: 'bold',
+  },
+  firstListItemText: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    color: '#de5006',
+  }
 };
 
 export default NavigationDrawer;
