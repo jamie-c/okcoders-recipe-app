9bc1741b7312cdf5f0bf8362610a34f27b767859
Jamie Conway <jamie@jpconway.com>
2023-09-29 22:03:55 -0500

    fix setting random link

--- src/components/FeelingHungryButton.jsx
+++ src/components/FeelingHungryButton.jsx
@@ -1,12 +1,29 @@
 import { Button } from '@mui/material'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const FeelingHungryButton = () => {
+    const [recipe, setRecipe] = useState('')
+    const [loading, setLoading] = useState(true)
+
+    const fetchRandomRecipe = async () => {
+        setLoading(true)
+        const res = await fetch('/api/recipes/random')
+        const data = await res.json()
+        setRecipe(data.recipe[0]._id)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchRandomRecipe()
+    }, [])
+
     return (
         <div>
             {' '}
-            <Link href="/api/recipes/random" passHref>
+            <Link href={`/recipes/${recipe}`} passHref onClick={fetchRandomRecipe}>
                 <Button
+                    disabled={loading}
                     variant="contained"
                     color="primary"
                     style={{ 
